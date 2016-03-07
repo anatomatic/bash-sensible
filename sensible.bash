7d193e0e1829655cc0df18739ed97512519d9faf
IMcA <ian.mcalpin@gmail.com>
2016-03-06 20:48:16 -0500

    Comment duplicated options; dial-in shortcuts

--- sensible.bash
+++ sensible.bash
@@ -13,7 +13,7 @@ set -o noclobber
 shopt -s checkwinsize
 
 # Automatically trim long paths in the prompt (requires Bash 4.x)
-PROMPT_DIRTRIM=2
+# PROMPT_DIRTRIM=2                      # on Bash 3.x
 
 # Allows space to complete and expand !$ eg:
 # $ ls Projects
@@ -34,20 +34,20 @@ bind "set show-all-if-ambiguous on"
 ## SANE HISTORY DEFAULTS ##
 
 # Append to the history file, don't overwrite it
-shopt -s histappend
+# shopt -s histappend                   # already exists in 'bash/.bashrc'
 
 # Save multi-line commands as one command
 shopt -s cmdhist
 
 # Record each line as it gets issued
-PROMPT_COMMAND='history -a'
+# PROMPT_COMMAND='history -a'           # already exists in 'bash/.bashrc'
 
 # Huge history. Doesn't appear to slow things down, so why not?
-HISTSIZE=500000
-HISTFILESIZE=100000
+# HISTSIZE=500000                       # already exists in 'bash/.bashrc'
+# HISTFILESIZE=100000                   # already exists in 'bash/.bashrc'
 
 # Avoid duplicate entries
-HISTCONTROL="erasedups:ignoreboth"
+# HISTCONTROL="erasedups:ignoreboth"    # already exists in 'bash/.bashrc'
 
 # Don't record some commands
 export HISTIGNORE="&:[ ]*:exit:ls:bg:fg:history:clear"
@@ -57,8 +57,8 @@ HISTTIMEFORMAT='%F %T '
 
 # Enable incremental history search with up/down arrows (also Readline goodness)
 # Learn more about this here: http://codeinthehole.com/writing/the-most-important-command-line-tip-incremental-history-searching-with-inputrc/
-bind '"\e[A": history-search-backward'
-bind '"\e[B": history-search-forward'
+# bind '"\e[A": history-search-backward' # already exists in 'bash/.bashrc'            
+# bind '"\e[B": history-search-forward'  # already exists in 'bash/.bashrc'        
 bind '"\e[C": forward-char'
 bind '"\e[D": backward-char'
 
@@ -74,15 +74,15 @@ shopt -s cdspell 2> /dev/null
 # This defines where cd looks for targets
 # Add the directories you want to have fast access to, separated by colon
 # Ex: CDPATH=".:~:~/projects" will look for targets in the current working directory, in home and in the ~/projects folder
-CDPATH="."
+CDPATH=".:~:~/Dropbox/3-WRITING:~/Dropbox/6-CODE"
 
 # This allows you to bookmark your favorite places across the file system
 # Define a variable containing a path and you will be able to cd into it regardless of the directory you're in
 shopt -s cdable_vars
 
 # Examples:
-# export dotfiles="$HOME/dotfiles"
-# export projects="$HOME/projects"
+export dotfiles="$HOME/.dotfiles"
+export projects="$HOME/Dropbox/2-PROJECTS"
 # export documents="$HOME/Documents"
-# export dropbox="$HOME/Dropbox"
+export dropbox="$HOME/Dropbox"
 
